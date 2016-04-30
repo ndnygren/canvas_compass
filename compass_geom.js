7cbedca312743c92a49b0adca4636690ac993a78
Nick Nygren <nnygren@shaw.ca>
2016-04-30 12:57:53 -0600

    parser working on test string

--- compass_geom.js
+++ compass_geom.js
@@ -9,24 +9,70 @@ function assocFoldr(list, func) {
 	}
 	return output
 }
+function zippr(lhs, rhs, func) {
+	if (lhs.length != rhs.length) {throw("zipping non-equal lengths");}
+	var output = [];
+
+	for (var i = 0; i < lhs.length; i++) {
+		output.push(func(lhs[i],rhs[i]));
+	}
+
+	return output;
+}
 
 function CTNode(input) {
-	this.name = input;
+	this.name = input.replace(/[ \t]+$/, "").replace(/^[(), \t]+/,"");
 	this.child = [];
+
+	this.equalTo = function(rhs) {
+		if (this.name != rhs.name) { return false; }
+		if (this.child.length != rhs.child.length) { return false; }
+		if (this.child.length == 0) { return true; }
+		return assocFoldr(zippr(this.child, rhs.child, function(a,b) {
+			return a.equalTo(b);
+		}), function (a,b) { return a && b; } );
+	}
 }
 
 function ConstructParse() {
-	this.read = function() {
-		var output = new CTNode("top");
-		output.child.push(new CTNode("banana"));
-		output.child[0].child.push(new CTNode("orange"));
-		output.child.push(new CTNode("avacado"));
-		return output;
+	this.nextToken = function(str, current, tok) {
+		for (var i = current + 1; i < str.length; i++) {
+			if (tok.indexOf(str[i]) >= 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	this.read = function(input) {
+		var tree = new CTNode("top");
+		var stack = [tree];
+		var tokens = [",", "(", ")"];
+		var str = " mid   (  banana , orange  (  pineapple )  , avacado ) ";
+		var cur = 0, next = this.nextToken(str, cur, tokens);
+		var newtoken = "";
+		for (cur = 0; cur < str.length && cur >= 0; next = this.nextToken(str, cur, tokens)) {
+			newtoken = next > -1 ? str.substring(cur, next) : "";
+			if (newtoken.replace(/^[,() \t]+/,"") != "") {
+				stack[stack.length -1].child.push(new CTNode(newtoken));
+			}
+			if (str[next] == "(") {
+				stack.push(stack[stack.length-1].child[stack[stack.length-1].child.length -1]);
+			}
+			else if (str[next] == ")") {
+				if (stack.length == 1) { throw("Negative Bracket Balance."); }
+				stack.pop();
+			}
+			cur = next;
+		}
+
+		if (stack.length != 1) { throw("stack size is " + stack.length + ". This probably means bracket imbalance."); }
+		return stack[0];
 	}
 
 	this.htmlDebug = function(tree) {
 		var pa = this;
-		var output = tree.name;
+		var output = "\"" + tree.name + "\"";
 		if (tree.child.length > 0) {
 			output += "<ul>";
 			output += assocFoldr(tree.child.map(function (x) {
